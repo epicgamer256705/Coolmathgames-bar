086b699b4fba2bd72d5929664adfd17d71a1eb70
epicgamer256705 <86577845+epicgamer256705@users.noreply.github.com>
2022-10-13 18:08:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function newgamebar(image) {
             "padding": "8px",
             "display": "flex"
         }).attr("class", "gamebar").append(
-            $("<img>").attr("src", "https://www.coolmathgames.com/pwa/images/icon-128x128.png").css("margin-right", "8px")
+            $("<img>").attr("src", "https://www.coolmathgames.com/pwa/images/icon-128x128.png")
         ).append(
             $("<p>").text("GAME NAME").attr("class", "gamebar-name").css({
                 "position": "relative",
@@ -52,7 +52,8 @@ function newgamebar(image) {
             $("<p>").click(togglefull).text("test").attr("class", "gamebar-fulltoggle").css({
                 "position": "sticky",
                 "left": "100%",
-                "cursor": "pointer"
+                "cursor": "pointer",
+                "margin-left": "8px"
             }).append(
 //                 $("<svg>").html(
 //                     '<path fill="white" d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/>'
