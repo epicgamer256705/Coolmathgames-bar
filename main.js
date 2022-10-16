ac9949014a9257ff11efc966cdf783243e0e41b5
epicgamer256705 <86577845+epicgamer256705@users.noreply.github.com>
2022-10-16 09:12:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -37,6 +37,15 @@ const cssCode = `
     width: 18px;
     height: 18px;
 }
+
+.field-game {
+    display: block;
+}
+
+#html5game {
+    transition-duration: 400ms;
+    transition-timing-function: ease-in-out;
+}
 `
 
 const gamebarInnerHtmlCode = `
@@ -74,57 +83,26 @@ if (window.location.host == "www.coolmathgames.com" && !window.location.pathname
 }
 
 function newgamebar(image) {
-    // Create the gamebar
-//     $(".field-game").prepend(
-//         $("<div>").attr("class", "gamebar").append(
-//             $("<img>").attr("src", "https://www.coolmathgames.com/pwa/images/icon-128x128.png")
-//         ).append(
-//             $("<p>").text("GAME NAME").attr("class", "gamebar-name")
-//         ).append(
-//             $("<div>").attr("class", "gamebar-right").append(
-//                 $("<p>").attr("onclick", "togglemax()").attr("class", "gamebar-toggle").append(
-//                     $("<svg>").html(
-//                         '<path fill="white" d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/>'
-//                     ).attr("viewBox", "0 0 448 512")
-//                 )
-//             ).append(
-//                 $("<p>").attr("onclick", "togglefull()").attr("class", "gamebar-fulltoggle").css({
-//                     "cursor": "pointer"
-//                 }).append(
-//                     $("<svg>").attr("class", "gamebar-fulltoggle-full").html(
-//                         '<path fill="white" d="M512 160l-32 32-52-52L328 240l-56-56L372 84 320 32 352 0H512V160zM0 352l32-32 52 52L184 272l56 56L140 428l52 52-32 32H0V352z"></path>'
-//                     ).attr("viewBox", "0 0 512 512")
-//                 ).append(
-//                     $("<svg>").attr("class", "gamebar-fulltoggle-unfull").html(
-//                         '<path fill="white" d="M272 80l32-32 52 52L456 0l56 56L412 156l52 52-32 32H272V80zM240 432l-32 32-52-52L56 512 0 456 100 356 48 304l32-32H240V432z"></path>'
-//                     ).attr("viewBox", "0 0 512 512").css({
-//                         "display": "none"
-//                     })
-//                 )
-//             )
-//         )
-//     )
-    
-    var fieldGame = document.querySelectorAll(".field-game")[0]
+    const fieldGame = document.querySelectorAll(".field-game")[0]
 
-    // Add gamebar element to field-game
+    // Create and add gamebar element to field-game
     var gamebarElem = document.createElement("div")
     gamebarElem.className = "gamebar"
     gamebarElem.innerHTML = gamebarInnerHtmlCode
     
     fieldGame.prepend(gamebarElem)
     
-    // Add css element to field-game
+    // Create and add style element to field-game
     var cssElem = document.createElement("style")
     cssElem.innerHTML = cssCode
     
     fieldGame.appendChild(cssElem)
     
-    // Make max svg show
-    $(".gamebar-toggle")[0].outerHTML += " "
+//     // Make max svg show
+//     $(".gamebar-toggle")[0].outerHTML += " "
     
-    // Make full and unfull svg show
-    $(".gamebar-fulltoggle")[0].outerHTML += " "
+//     // Make full and unfull svg show
+//     $(".gamebar-fulltoggle")[0].outerHTML += " "
 
     // Get the game name for the gamebar
     const arr = window.location.pathname.replace("/0-", "").replaceAll("-", " ").split(" ");
@@ -135,15 +113,6 @@ function newgamebar(image) {
 
     // Set the game name for the gamebar
     $(".gamebar-name").text(arr.join(" "))
-
-    // Change gamediv display css property
-    $(".field-game").css("display", "block")
-    
-    // Modify css of game iframe
-    $("#html5game").css({
-        "transition-duration": "400ms",
-        "transition-timing-function": "ease-in-out"
-    })
     
     // Change iframe1 to iframe
     try {
