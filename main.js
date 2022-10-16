cb96832b58840198e65b666d0a207b9df19d5ee8
epicgamer256705 <86577845+epicgamer256705@users.noreply.github.com>
2022-10-16 09:08:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -39,25 +39,23 @@ const cssCode = `
 }
 `
 
-const htmlCode = `
-<div class="gamebar">
-    <img src="https://www.coolmathgames.com/pwa/images/icon-128x128.png">
-	<p class="gamebar-name">GAME NAME</p>
-	<div class="gamebar-right">
-		<p onclick="togglemax()" class="gamebar-toggle">
-			<svg viewBox="0 0 448 512">
-				<path fill="white" d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"></path>
-			</svg>
-		</p>
-		<p onclick="togglefull()" class="gamebar-fulltoggle">
-			<svg class="gamebar-fulltoggle-full" viewBox="0 0 512 512">
-				<path fill="white" d="M512 160l-32 32-52-52L328 240l-56-56L372 84 320 32 352 0H512V160zM0 352l32-32 52 52L184 272l56 56L140 428l52 52-32 32H0V352z"></path>
-			</svg>
-			<svg class="gamebar-fulltoggle-unfull" viewBox="0 0 512 512" style="display: none;">
-				<path fill="white" d="M272 80l32-32 52 52L456 0l56 56L412 156l52 52-32 32H272V80zM240 432l-32 32-52-52L56 512 0 456 100 356 48 304l32-32H240V432z"></path>
-			</svg>
-		</p>
-	</div>
+const gamebarInnerHtmlCode = `
+<img src="https://www.coolmathgames.com/pwa/images/icon-128x128.png">
+<p class="gamebar-name">GAME NAME</p>
+<div class="gamebar-right">
+	<p onclick="togglemax()" class="gamebar-toggle">
+		<svg viewBox="0 0 448 512">
+			<path fill="white" d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"></path>
+		</svg>
+	</p>
+	<p onclick="togglefull()" class="gamebar-fulltoggle">
+		<svg class="gamebar-fulltoggle-full" viewBox="0 0 512 512">
+			<path fill="white" d="M512 160l-32 32-52-52L328 240l-56-56L372 84 320 32 352 0H512V160zM0 352l32-32 52 52L184 272l56 56L140 428l52 52-32 32H0V352z"></path>
+		</svg>
+		<svg class="gamebar-fulltoggle-unfull" viewBox="0 0 512 512" style="display: none;">
+			<path fill="white" d="M272 80l32-32 52 52L456 0l56 56L412 156l52 52-32 32H272V80zM240 432l-32 32-52-52L56 512 0 456 100 356 48 304l32-32H240V432z"></path>
+		</svg>
+	</p>
 </div>
 `
 
@@ -107,17 +105,20 @@ function newgamebar(image) {
 //         )
 //     )
     
+    var fieldGame = document.querySelectorAll(".field-game")[0]
+
     // Add gamebar element to field-game
     var gamebarElem = document.createElement("div")
-    gamebarElem.outerHTML = htmlCode
+    gamebarElem.className = "gamebar"
+    gamebarElem.innerHTML = gamebarInnerHtmlCode
     
-    document.querySelectorAll(".field-game")[0].prepend(gamebarElem)
+    fieldGame.prepend(gamebarElem)
     
     // Add css element to field-game
     var cssElem = document.createElement("style")
     cssElem.innerHTML = cssCode
     
-    document.querySelectorAll(".field-game")[0].appendChild(cssElem)
+    fieldGame.appendChild(cssElem)
     
     // Make max svg show
     $(".gamebar-toggle")[0].outerHTML += " "
