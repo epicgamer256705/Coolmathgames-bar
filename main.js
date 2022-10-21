6fa3263af01679edae9a4fea35c40851d89981e7
epicgamer256705 <86577845+epicgamer256705@users.noreply.github.com>
2022-10-21 13:27:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,12 @@ const cssCode = `
     padding: 0.15em;
 }
 
+.gamebar-randomgame > svg {
+    height: 1em;
+    width: 1em;
+    padding: 0.15em;
+}
+
 .gamebar-fulltoggle[data-icon="full"] .gamebar-fulltoggle-full {
     display: block;
 }
@@ -95,7 +101,7 @@ const cssCode = `
     border-radius: 0px 0px 5px 5px;
     padding: 4px;
     padding-bottom: 0px;
-    width: calc(80px + 4em);
+    width: calc(80px + 6em);
 }
 
 .gamebar-search-results > a {
@@ -113,6 +119,11 @@ const gamebarInnerHtmlCode = `
 		<input class="gamebar-search-input" oninput="onsearchbarchange()" placeholder="Search">
 		<div class="gamebar-search-results"></div>
 	</div>
+	<p onclick="randomgame()" class="gamebar-randomgame">
+		<svg viewBox="0 0 512 512">
+			<path fill="white" d="M403.8 34.4c12-5 25.7-2.2 34.9 6.9l64 64c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-64 64c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V160H352c-10.1 0-19.6 4.7-25.6 12.8L284 229.3 244 176l31.2-41.6C293.3 110.2 321.8 96 352 96h32V64c0-12.9 7.8-24.6 19.8-29.6zM164 282.7L204 336l-31.2 41.6C154.7 401.8 126.2 416 96 416H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96c10.1 0 19.6-4.7 25.6-12.8L164 282.7zm274.6 188c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V416H352c-30.2 0-58.7-14.2-76.8-38.4L121.6 172.8c-6-8.1-15.5-12.8-25.6-12.8H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96c30.2 0 58.7 14.2 76.8 38.4L326.4 339.2c6 8.1 15.5 12.8 25.6 12.8h32V320c0-12.9 7.8-24.6 19.8-29.6s25.7-2.2 34.9 6.9l64 64c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-64 64z"></path>
+		</svg>
+	</p>
 	<p onclick="togglemax()" class="gamebar-toggle">
 		<svg viewBox="0 0 448 512">
 			<path fill="white" d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"></path>
@@ -126,7 +137,6 @@ const gamebarInnerHtmlCode = `
 			<path fill="white" d="M272 80l32-32 52 52L456 0l56 56L412 156l52 52-32 32H272V80zM240 432l-32 32-52-52L56 512 0 456 100 356 48 304l32-32H240V432z"></path>
 		</svg>
 	</p>
-	<p onclick="randomgame()">random</p>
 </div>
 `
 
@@ -344,6 +354,9 @@ function changegame(url) {
 
 function randomgame() {
     var game = games[Math.floor(Math.random() * (games.length - 1))]
-    if (game.type != "html5") return
+    if (game.type !== "html5") {
+        game = games[Math.floor(Math.random() * (games.length - 1))]
+        randomgame()
+    }
     changegame(game.alias)
 }
